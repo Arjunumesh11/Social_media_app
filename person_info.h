84e1106b228117c3725fa0a264a5e54b34215ac1
Arjun U S <46133798+Arjunumesh11@users.noreply.github.com>
2019-09-08 14:09:55 +0530

    Add files via upload

---
+++ person_info.h
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<string>
+#include<queue>
+using namespace std;
+int count_of_user = 0;                //present count of user in data
+class person
+{
+public:
+	int getid()
+	{
+		return id;
+	}
+	string getname() { return name; }
+	bool ispassword(string x)
+	{
+		return (x == password);
+	}
+	void change(string x)
+	{
+		password = x;
+	}
+	person(string x, int i, string y) :name(x), password(y), id(i) {}
+	void edit_profile()
+	{
+		//fill in
+	}
+	void get_profile()
+	{
+		//fill in
+	}
+private:
+	string name, password,mail_id,phone_no;
+	int id;
+}; 
